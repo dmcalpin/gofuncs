f1f9db5772871c9f77c79c34620d8c5b21202762
Dave McAlpine <davemca@protonmail.com>
2021-12-15 19:03:55 -0500

    Made functions 'pure'. Added Filter()

--- lists/list_test.go
+++ lists/list_test.go
@@ -2,7 +2,7 @@ package lists
 
 import (
 	"testing"
-	
+
 	"github.com/stretchr/testify/suite"
 )
 
@@ -13,7 +13,7 @@ type ListSuite struct {
 	intList List[int]
 }
 
-func (s *ListSuite) SetupTest(){
+func (s *ListSuite) SetupTest() {
 	s.strList = List[string]{"str1", "str2"}
 	s.intList = List[int]{23, 45}
 }
@@ -29,133 +29,148 @@ func (s *ListSuite) TestIsGeneric() {
 func (s *ListSuite) TestForEachStrings() {
 	allStrs := ""
 
-	s.strList.ForEach(func(s string){
+	s.strList.ForEach(func(s string) {
 		allStrs += s + "|"
 	})
 
 	s.Require().Equal("str1|str2|", allStrs)
 }
 
-func (s *ListSuite) TestForEachInts(){
+func (s *ListSuite) TestForEachInts() {
 	sum := 0
 
-	s.intList.ForEach(func(i int){
+	s.intList.ForEach(func(i int) {
 		sum += i
 	})
 
 	s.Require().Equal(68, sum)
 }
 
-func (s *ListSuite) TestForEachIsChainable(){
+func (s *ListSuite) TestForEachIsChainable() {
 	sum := 0
 
 	s.intList.
-	ForEach(func(i int){
-		sum += i
-	}).
-	ForEach(func(i int){
-		sum += 2
-	})
+		ForEach(func(i int) {
+			sum += i
+		}).
+		ForEach(func(i int) {
+			sum += 2
+		})
 
 	s.Require().Equal(72, sum)
 }
 
 func (s *ListSuite) TestMapStrings() {
-	s.strList.Map(func(s string) string {
+	updatedList := s.strList.Map(func(s string) string {
 		return "mapped: " + s
 	})
 
-	s.Require().Equal(List[string]{"mapped: str1", "mapped: str2"}, s.strList)
+	s.Require().Equal(List[string]{"mapped: str1", "mapped: str2"}, updatedList)
 }
 
 func (s *ListSuite) TestMapInts() {
-	s.intList.Map(func(i int) int {
+	updatedList := s.intList.Map(func(i int) int {
 		return i * 2
 	})
 
-	s.Require().Equal(List[int]{46, 90}, s.intList)
+	s.Require().Equal(List[int]{46, 90}, updatedList)
 }
 
-func (s *ListSuite) TestMapIsChainable(){
-	s.intList.
-	Map(func(i int) int {
-		return i * 2
-	}).
-	Map(func(i int) int {
-		return i * 100
-	})
-	
-	s.Require().Equal(List[int]{4600, 9000}, s.intList)
+func (s *ListSuite) TestMapIsChainable() {
+	updatedList := s.intList.
+		Map(func(i int) int {
+			return i * 2
+		}).
+		Map(func(i int) int {
+			return i * 100
+		})
+
+	s.Require().Equal(List[int]{4600, 9000}, updatedList)
 }
 
-func (s *ListSuite) TestSortStrings(){
-	mediumStrList := List[string]{"z", "w", "a", "b"}
+func (s *ListSuite) TestSortStrings() {
+	strList := List[string]{"z", "w", "a", "b"}
 
-	mediumStrList.Sort(func(i, j int) bool {
-		return mediumStrList[i] < mediumStrList[j]
-	})
-
-	s.Require().Equal(List[string]{"a", "b", "w", "z"}, mediumStrList)
-}
-
-func (s *ListSuite) TestSortInts(){
-	mediumIntList := List[int]{2, 8, 21, 93, 2013, 92, 0}
-
-	mediumIntList.Sort(func(i, j int) bool {
-		return mediumIntList[i] < mediumIntList[j]
+	ascList := strList.Sort(func(a, b string) bool {
+		return a < b
 	})
 
-	s.Require().Equal(List[int]{0, 2, 8, 21, 92, 93, 2013}, mediumIntList)
-}
+	s.Require().Equal(List[string]{"a", "b", "w", "z"}, ascList)
 
-func (s *ListSuite) TestSortIsChainable(){
-	mediumStrList := List[string]{"z", "w", "a", "b"}
-
-	mediumStrList.Sort(func(i, j int) bool {
-		return mediumStrList[i] < mediumStrList[j]
-	}).
-	Map(func(elem string) string{
-		return elem + elem
+	descList := strList.Sort(func(a, b string) bool {
+		return a > b
 	})
 
-	s.Require().Equal(List[string]{"aa", "bb", "ww", "zz"}, mediumStrList)
+	s.Require().Equal(List[string]{"z", "w", "b", "a"}, descList)
 }
 
-func (s *ListSuite) TestLen(){
+func (s *ListSuite) TestSortInts() {
+	intList := List[int]{2, 8, 21, 93, 2013, 92, 0}
+
+	updatedList := intList.Sort(func(a, b int) bool {
+		return a < b
+	})
+
+	s.Require().Equal(List[int]{0, 2, 8, 21, 92, 93, 2013}, updatedList)
+}
+
+func (s *ListSuite) TestSortIsChainable() {
+	strList := List[string]{"z", "w", "a", "b"}
+
+	updatedList := strList.
+		Sort(func(a, b string) bool {
+			return a < b
+		}).
+		Map(func(elem string) string {
+			return elem + elem
+		})
+
+	s.Require().Equal(List[string]{"aa", "bb", "ww", "zz"}, updatedList)
+}
+
+func (s *ListSuite) TestLen() {
 	s.Require().Equal(2, s.strList.Len())
 }
 
-func (s *ListSuite) TestReduceSumInts(){
-	mediumIntList := List[int]{1,2,3,4,5}
-	
-	reducedVal := mediumIntList.Reduce(func (prev, curr int) int{
+func (s *ListSuite) TestReduceSumInts() {
+	intList := List[int]{1, 2, 3, 4, 5}
+
+	reducedVal := intList.Reduce(func(prev, curr int) int {
 		return prev + curr
 	}, 0)
 
 	s.Require().Equal(15, reducedVal)
 }
 
+func (s *ListSuite) TestReduceFlattenSlice() {
+	intSliceList := List[[]int]{[]int{1, 2}, []int{3, 4}, []int{5, 6}}
 
-func (s *ListSuite) TestReduceFlattenSlice(){
-	intSliceList := List[[]int]{[]int{1,2}, []int{3,4}, []int{5, 6}}
-	
-	reducedVal := intSliceList.Reduce(func (prev, curr []int) []int{
+	reducedVal := intSliceList.Reduce(func(prev, curr []int) []int {
 		return append(prev, curr...)
 	}, nil)
 
-	s.Require().Equal([]int{1,2,3,4,5,6}, reducedVal)
+	s.Require().Equal([]int{1, 2, 3, 4, 5, 6}, reducedVal)
 }
 
-func (s *ListSuite) TestReduceNoElems(){
-	mediumIntList := List[int]{}
-	
-	reducedVal := mediumIntList.Reduce(func (prev, curr int) int{
+func (s *ListSuite) TestReduceNoElems() {
+	intList := List[int]{}
+
+	reducedVal := intList.Reduce(func(prev, curr int) int {
 		return prev + curr
 	}, 23)
 
 	s.Require().Equal(23, reducedVal)
 }
 
+func (s *ListSuite) TestFilterShortStrings() {
+	strList := List[string]{"a", "dog", "walks", "to", "the", "park"}
+
+	updatedList := strList.Filter(func(word string) bool {
+		return len(word) > 3
+	})
+
+	s.Require().Equal(List[string]{"walks", "park"}, updatedList)
+}
 
 func TestList(t *testing.T) {
 	suite.Run(t, new(ListSuite))
--- lists/lists.go
+++ lists/lists.go
@@ -1,9 +1,5 @@
 package lists
 
-import (
-	"sort"
-)
-
 // List is a slice of anything,
 // from which you can call many
 // different functions
@@ -25,20 +21,40 @@ func (l List[T]) ForEach(f func(T)) List[T] {
 // the parameter, assigning the result
 // to the current of the List
 func (l List[T]) Map(f func(T) T) List[T] {
-	for i, elem := range l {
-		l[i] = f(elem)
+	tmp := List[T]{}
+	for _, elem := range l {
+		tmp = append(tmp, f(elem))
 	}
 
-	return l
+	return tmp
 }
 
 // Sort is a wrapper for sort.Slice.
-// It takes a "less" function as its parameter
-// which is passed to sort.Slice
-func (l List[T]) Sort(f func(int, int) bool) List[T] {
-	sort.Slice(l, f)
+// It takes a compare function as its parameter
+// which is used to determine order.
+// a < b for ascending order
+// a > b for descending order
+func (l List[T]) Sort(compare func(T, T) bool) List[T] {
+	tmp := make(List[T], len(l))
+	copy(tmp, l)
 
-	return l
+	tmp = bubbleSort(tmp, compare)
+
+	return tmp
+}
+
+// I know... just need an alternative to sort.Slice which
+// has a compare function that isn't indexes
+// TODO: use a better algorithm
+func bubbleSort[T any](list List[T], compare func(T, T) bool) List[T] {
+	for i := 0; i < len(list)-1; i++ {
+		for j := 0; j < len(list)-i-1; j++ {
+			if compare(list[j+1], list[j]) {
+				list[j], list[j+1] = list[j+1], list[j]
+			}
+		}
+	}
+	return list
 }
 
 // Len wraps the built-in len()
@@ -53,15 +69,32 @@ func (l List[T]) Len() int {
 // of the current iteration. A single
 // accumulated value is returned.
 // This method is not chainable
-func (l List[T]) Reduce(f func (prev, curr T) T, initialValue T) T {
+func (l List[T]) Reduce(f func(prev, curr T) T, initialValue T) T {
 	accumulator := initialValue
 	if l.Len() == 0 {
 		return accumulator
 	}
-	
+
 	for _, elem := range l {
 		accumulator = f(accumulator, elem)
 	}
 
 	return accumulator
+}
+
+// Filter calls a filter function for
+// each element in the List. If the
+// return value of the filter function is
+// true, then the element is kept in
+// the List, otherwise it is removed
+func (l List[T]) Filter(f func(T) bool) List[T] {
+	tmp := List[T]{}
+	for _, elem := range l {
+		if f(elem) {
+			tmp = append(tmp, elem)
+		}
+
+	}
+
+	return tmp
 }
